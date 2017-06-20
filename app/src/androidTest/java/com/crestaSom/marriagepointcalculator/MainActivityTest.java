26d3b9b87f3eb9bba6767aad8faf67be139004e2
Som Shrestha <Som Shrestha>
2017-06-20 21:50:31 +0545

    Major Update, Final Touch Required

---
+++ app/src/androidTest/java/com/crestaSom/marriagepointcalculator/MainActivityTest.java
@@ -0,0 +1,22 @@
+package com.crestaSom.marriagepointcalculator;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by user on 6/11/2017.
+ */
+public class MainActivityTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
---
+++ app/src/androidTest/java/com/crestaSom/marriagepointcalculator/ScoreCardTest.java
@@ -0,0 +1,19 @@
+package com.crestaSom.marriagepointcalculator;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by user on 6/7/2017.
+ */
+public class ScoreCardTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+}
